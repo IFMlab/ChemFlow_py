b71fbde37cd8dd7bfabcb4e7333264c1f662c90a
lmonari5 <101123231+lmonari5@users.noreply.github.com>
2022-08-28 16:55:51 +0200

    Update chemflow.py
    added the command line part here

--- modules/chemflow.py
+++ modules/chemflow.py
@@ -26,6 +26,7 @@ from alive_progress import alive_bar
 import multiprocessing as mp
 from functools import partial
 # CHEMFLOW modules
+import chemflow_parse
 import run
 import bound_shape
 import consensus_functions
@@ -1321,3 +1322,45 @@ class Rescore(Step):
         super().__init__(receptor, ligand, protocol=protocol, program=program, header=header, scheduler=scheduler,
                          verbose=verbose, molecule_job=molecule_job)
         self.type = 'rescore'
+
+        
+if __name__ == "__main__":
+    var = chemflow_parse.dockflow_parse()
+    if var['method'] == 'dock':
+        docking = Dock(var['receptor'], var['ligand'], program=var['program'], protocol=var['protocol'],
+                          verbose=var['verbose'], header=var['header'], scheduler=var['job_scheduler'],
+                          molecule_job=var['molecule_job'])
+    else:
+        docking = Rescore(var['receptor'], var['ligand'], program=var['program'], protocol=var['protocol'],
+                          verbose=var['verbose'], header=var['header'], scheduler=var['job_scheduler'],
+                          molecule_job=var['molecule_job'])
+    if var['load']:
+        previous = load(var['load'], n=0)
+        docking.data = previous.data
+    for key in docking.data:
+        if key in var and var[key]:
+            docking.data[key] = var[key]
+    if var['config']:
+        module = importlib.import_module(var['program'])
+        docking.data = module.read_config(var['config'])
+    if var['crystal']:
+        docking.bounding_shape(var['crystal'], 'both')
+    docking.check_input()
+    chemflow_parse.resume(docking, var)
+    if docking.hpc_scheduler and not docking.molecule_job:
+        docking.molecule_job = int(input(f'How many docking for {docking.scheduler} job? '))
+    start = input('Do you want to continue? ([y]/n) ')
+    if 'n' in start.lower():
+        exit()
+    if not var['run_only']:
+        docking.setup(overwrite=var['overwrite'])
+    if var['config']:
+        shutil.copyfile(var['config'], docking.protocol + module.config_file_name)
+    if var['write_only']:
+        exit()
+    computed = False
+    k_poses = var['k_poses']
+    if var['postprocess']:
+        computed = True
+        k_poses = var['k_poses']
+    docking.run(keep_poses=k_poses, computed=computed)
